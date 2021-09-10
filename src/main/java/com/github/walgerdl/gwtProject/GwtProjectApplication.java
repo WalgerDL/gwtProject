f0fd660953b416aefeae7ebe509239e5c3f1a4eb
WalgerDL <mirmic.walger@gmail.com>
2021-09-11 00:39:11 +0300

    Initial commit gwtProject V 1.0

---
+++ src/main/java/com/github/walgerdl/gwtProject/GwtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.walgerdl.gwtProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwtProjectApplication.class, args);
+	}
+
+}
